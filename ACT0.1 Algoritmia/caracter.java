23be32b26c0a3b8f60a0c657d7dc52fcbec5d43f
agp-00 <agp00@iesemilidarder.com>
2023-10-02 16:37:47 +0200

    break secuencia

--- ACT0.1 Algoritmia/caracter.java
+++ ACT0.1 Algoritmia/caracter.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class caracter {
    public static void main(String []args) {
     Scanner frase = new Scanner(System.in);
+    System.out.println("Si la frase contiene una 'X' se detendrá el programa.");
     System.out.println("Escoja una frase a analizar:");
     String frase1 = frase.nextLine();
     char ocurrencia = 'l';
     int count = 0;
      
     for (int i = 0; i < frase1.length(); i++) {
+        if (frase1.charAt(i) == 'X') {
+            break;
+        }
         if (frase1.charAt(i) == ocurrencia && frase1.charAt(i+1) == 'a') {
             count++;
         }
