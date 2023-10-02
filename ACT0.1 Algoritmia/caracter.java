255a950657d810ed1daed124a7dfd795472c2da6
agp-00 <agp00@iesemilidarder.com>
2023-10-02 17:25:02 +0200

    creación variable en caso de mayuscula

--- ACT0.1 Algoritmia/caracter.java
+++ ACT0.1 Algoritmia/caracter.java
@@ -7,6 +7,7 @@ public class caracter {
     System.out.println("Escoja una frase a analizar:");
     String frase1 = frase.nextLine();
     char ocurrencia = 'l';
+    char ocurrencia2 = 'L';
     int count = 0;
      
     for (int i = 0; i < frase1.length(); i++) {
@@ -15,7 +16,7 @@ public class caracter {
         }
         if (frase1.charAt(i) == ocurrencia && frase1.charAt(i+1) == 'a' || frase1.charAt(i+1) == 'A') {
             count++;
-        } else if (frase1.charAt(i) == 'L' && frase1.charAt(i+1) == 'a' || frase1.charAt(i+1) == 'A') {
+        } else if (frase1.charAt(i) == ocurrencia2 && frase1.charAt(i+1) == 'a' || frase1.charAt(i+1) == 'A') {
             count++;
         }
     }
