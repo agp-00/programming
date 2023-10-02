c9d29c3e9132d3bc3e7ab726b08eb85a90ab26e0
agp-00 <agp00@iesemilidarder.com>
2023-10-02 16:33:46 +0200

    secuencia en string

---
+++ ACT0.1 Algoritmia/caracter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class caracter {
+   public static void main(String []args) {
+    Scanner frase = new Scanner(System.in);
+    System.out.println("Escoja una frase a analizar:");
+    String frase1 = frase.nextLine();
+    char ocurrencia = 'l';
+    int count = 0;
+     
+    for (int i = 0; i < frase1.length(); i++) {
+        if (frase1.charAt(i) == ocurrencia && frase1.charAt(i+1) == 'a') {
+            count++;
+        }
+    }
+    System.out.println("La ocurrencia 'la' se repite " + count + " veces.");
+   }
+}
