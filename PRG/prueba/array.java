630ddba7e37ba228d8390eb238871fc4167d4cdb
agp-00 <agp00@iesemilidarder.com>
2023-10-18 17:56:57 +0200

    resuelto

--- PRG/prueba/array.java
+++ PRG/prueba/array.java
@@ -1,6 +1,6 @@
 package PRG.prueba;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array {
@@ -9,18 +9,26 @@ public class array {
         System.out.println("Introduce la longitud del array:");
         int N = longi.nextInt();
         int[] array = new int[N];
+        double suma = 0;
 
         for(int i = 0 ; i < N; i++){
             System.out.println("Introduce el siguiente valor que habrá en el array:");
             array[i] = longi.nextInt();
+            suma = suma + array[i];
             if (i == N-1) {
                 System.out.println("Los elementos del array son los siguientes:");
                 for (i = 0; i < N; i++) {
                     System.out.print(array[i] + " ");
                 }
+                System.out.println("\nLa media de los numeros del array es: " + (suma/N));
+                Arrays.sort(array);
+                System.out.println("El valor mínimo del array es " + array[0]);
+                System.out.println("El valor máximo del array es " + array[N-1]);
+                double j;
             }
         }
         
+
         longi.close();
     }
 }
